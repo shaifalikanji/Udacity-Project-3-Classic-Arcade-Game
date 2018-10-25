dc967afa2148755beee43e398304006689c0632c
Shaifali Kanji <shuff_786@hotmail.com>
2018-10-25 13:45:26 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Udacity-Project-3-Classic-Arcade-Game
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+
+Classic Arcade Game: 
+
+•	In this game you have a Player and Enemies (Bugs). The goal of the player is to reach the water, without colliding into any one of the enemies. 
+•	The player can move left, right, up and down. 
+•	The enemies move in varying speeds on the paved block portion of the scene. 
+•	Once the player collides with an enemy, the game is reset and the player moves back to the start square.
+•	Once the player reaches the water the game is won. 
+•	Your goal is to make this game functional and error free.
+Source: Udacity Project Rubric.
+
+Required knowledge and understanding of basics JavaScript concepts:
+1.	Plain (Vanilla) JavaScript language
+2.	Object Oriented Programming using JavaScript
+3.	Understanding of: objects, constructs, prototypes (prototypal inheritance) and (“this” keyword).
+
+Startup summary:
+
+•	Plan your code first: so, your code is structured, clean, precise and easy to understand.
+•	Please note: when creating objects focus on the properties (characteristics of the object) and methods (using functions define what exactly does your object do). Please choose either Plain JavaScript/ES6 JavaScript features to code your game.
+•	If you choose to use ES6, create one object class and constructor. Then use the “new” keyword and create new objects for the (player) and (enemy). The new objects will inherit properties and methods(functions) from the original object class. Then create additional functions(methods)/properties for each “new object” defined.
+
+
+
+Coding requirements for the game:
+
+Step 1: 
+
+1. Open the html.index file, right click and select “inspect source” – you will notice a list of  “Uncaught reference errors” for functions/variables/arrays that are not defined. Start with defining them first until all errors are removed.
+
+Step 2: Player 
+
+1.	define player – create class and instantiate player
+2.	Sprite - this is the URL of the image you will use in the game
+3.	This. X = x axis = horizontal position
+4.	This. Y = y axis = vertical position
+5.	You want the player to cross the road without colliding with the enemy
+6.	moves with user input (handle Input () function) – allows you to use the arrow keys (up, down, left, right) to play the game. 
+7.	Anything above 0, the player will fall off the game board. This can’t happen
+8.	player should move up or down by changing the value of its y property
+9.	can use a “switch statement” with different values for the player to move around but remain on the board game throughout the game while using up, down, left and right arrow keys( defined in event listeners)
+10.	Check collisions: create a collisions function ():  checks the colliding numbers on the player and enemy, if they end up colliding a collision happens(when enemy/player position on the game board overlap. Function can be used on player or enemy or both. If the player collides with the enemy or the enemy collides with the player a collision happens. 
+11.	When the collision happens, the player gets bumped back down to its original position on the green grass.
+12.	Check who won: create an update () function and alert () indicating player is not out of bounds, has reached the top of the game board without colliding and with an enemy and “won” the game.
+13.	Reset your game board: create a reset function () which re-sets the entire game board including the player and enemies.
+
+
+Step 3: Enemy 
+
+1.	define enemy – create class and instantiate enemy
+2.	Sprite - this is the URL of the image you will use in the game
+3.	This. X = x axis = horizontal position
+4.	This. Y = y axis = vertical position
+5.	Anything above 0, the enemy will fall off the game, but enemy must keep appear again immediately within the loop of the game board.
+6.	You can change the speed of your enemy: each enemy: so, the move at different speeds
+Choose random speeds for start of the game, and repeated looping of the enemy
+7.	objects move on their own (update () function) causes the enemy to move
+8.	check for collisions function ():  checks the colliding numbers on the player and enemy, if they end up colliding a collision happens. Function can be used on player or enemy or both. If the player collides with the enemy or the enemy collides with the player a collision happens. 
+9.	
+
+
+Step 4: Both (Player && Enemy) 
+
+1.	move left and right by changing the x property
+2.	get player and enemy moving
+
+
+
---
+++ js/app.js
@@ -0,0 +1,238 @@
+/*canvas screen starts at 0/0 at the top left of the screen, to increase/decrease positions within game board, estimate
+various different pixels sizing in order for enemy and player to remain on game screen canvas*/
+/*note all numbers for positions are in PIXELS for the enemy and player objects*/
+//Player can not move off screen
+//Vehicles cross the screen
+//Vehicle-player collisions happen logically (not too early or too late)
+//Vehicle-player collision resets the game
+//Something happens when player wins// Enemies our player must avoid - congratulations alert!
+
+                                // STEP#1  ***** GAME START VARIABLE ***** //          
+
+var gameStart = true; //starts game when code runs
+
+                                        
+
+
+                                //STEP#2 ***** ENEMY OBJECT ******//
+                                //includes: enemy object, functions: update, render and collision b/w player/enemy //
+
+
+var Enemy = function(x,y) {  // Variables applied to each of our instances go here,
+    this.width = 55;//of enemy (bugs) image size
+    this. height = 75;
+    this.x = x; //this object: posiion of enemy on game board screen x axis horizontal at start point and during game
+    this.y = y; // this object: position of enemy on game board screen y axis vertical at start point and during game
+    this.sprite = 'images/enemy-bug.png'; // The image/sprite for our enemies, this uses helper provided to easily load images
+    this.collisions = false; //currently the collision is false - nothing happening yet
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+//updates game after every loop
+//dt makes the animation of the bugs walking on the canvas at speeds we can see
+
+Enemy.prototype.update = function(dt) { // multiply any movement by dt parameter ensures game runs at same speed for all computers.
+    this.x = this.x + 120 * dt; //enemies are not rendering until this is defined, for every loop iteration, bugs move at this position
+
+    
+    if (this.x > ctx.canvas.width + this.width){ // every time enemy (bug) loops off the canvas it will loop right back on
+        this.x = -190 * Math.floor(Math.random() * 3) + 7;
+    } else {
+        this.x += 140 * dt;
+    }
+
+//Collision between player and enemy when both end up on the same canvas square with
+//is there a collision or not (true or false)
+//https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
+
+
+
+
+    if (collisions(this.x, this.y, this.width, this.height, player.x, player.y, player.width, player.height)) {
+        this.collisions = true; //if the collision is true - replace the position of the player
+
+        if (player) { //if the player and enemy collide and a collision happens, replace the original position of the player
+            player.x = 300;
+            player.y = 408;
+
+        }
+    
+    } else { 
+        this.collisions = false; //if the enemy and player don collide, there is no collision
+    }
+
+
+
+};
+
+
+Enemy.prototype.render = function() { // Draw the enemy on the screen, required method for game
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+                                        
+                                       
+
+                                        
+
+                                //STEP#3 ***** PLAYER OBJECT ******//
+                    //includes: player object, functions: render, update, handleInput //
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+
+var Player = function(x,y) { //we passed x, y parameters here 
+    this.width = 75; //of player image size 
+    this.height = 85;
+    this.x = x; //this object: posiion of player on game board screen x axis horizontal at start point and during game
+    this.y = y; //this object: position of enemy on game board screen y axis vertical at start point and during game
+    this.sprite = 'images/char-pink-girl.png';
+    
+    
+
+}
+
+Player.prototype.render = function() { // Draw the player on the screen, required method for game
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
+// Update the players position, required method for game
+// Parameter: dt, a time delta between ticks
+//when player is at the top of the y axis (blue water lake) 
+Player.prototype.update = function(dt) { // multiply any movement by dt parameter ensures game runs at same speed for all computers.
+    if (gameStart && player.y < 30) {
+        gameStart = false;
+        congratulations(); //function for alert that congrats player for winning and reaching the top
+        reset();
+        
+    }
+
+
+};
+
+                              
+
+Player.prototype.handleInput = function(walk){ //this function will allow the player to move on the game canvas
+//work with various different numbers until player fits into each cube on the canvas and doesnt fall off canvas
+//this function is called below at the event listener based on which arrow keys the user is clicking (up/down/right/left)
+    
+    const xAxisHorizontal = 101; // on engine.js file line (#141)each column defined on the canvas is 101 pixels
+          yAxisVertical = 83; // on engine.js file line (#141) each row is 83 pixels
+                            //so we need to make sure each square on the canvas is 83, 101 for x axis and y axis 
+                            //so the player remains within the square and does not fall out either up, down, left or right 
+                            //of the canvas  
+                            //you add or minus - + to the current position of the player on the canvas 
+                            //eg: at players current position - walk left - 101 >= 0 - 100
+                            //ctx is the canvas object, which defines the width(505px) and height(606px) of the canvas 
+
+
+
+
+    if (walk === 'left' && this.x - xAxisHorizontal >= 0 - 100){ // player walks left on the x axis
+        this.x -= xAxisHorizontal; // if player is going left and x axis greater than 0 - 100 so player doesnt go off canvas
+
+    } else if (walk === 'right' && this.x + xAxisHorizontal < ctx.canvas.width - 100){ //player walks right on the x axis
+        this.x += xAxisHorizontal; // if player is going right and x axis must be less than canvas width so player doesnt go off canvas
+
+
+    } else if (walk === 'down' && this.y + yAxisVertical < ctx.canvas.height - 190){ // player walk down on the y axis
+        this.y += yAxisVertical; // if player walks down y axis must be less than canvas height, player doesnt go off canvas
+
+
+    } else if (walk === 'up' && this.y - yAxisVertical > 0 - player.height) { // player walks up on the y axis 
+        this.y -= yAxisVertical; // if player walks up y axis must be less than player height so player doesnt go off canvas
+
+    }
+
+}
+
+
+
+                                //STEP#4  ***** INSTANTIATE YOUR OBJECTS *****//
+                        //***** includes: enemy array, player variable, enemy loop using map *****//
+
+
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+//this loads the player on the game screen
+
+const enemyPositionOnCanvas = [57, 143, 233]; //array holds positions of the " 3 bugs" on the (y axis) on game canvas
+                                              //y axis numbers show up under console.log(allEnemies);
+const player = new Player(300, 408); //declaring player variable - call the Player function line # 39 and create a Player object
+                                     // 300px, 408px is the size of the image that fits in the canvas squares.
+
+var allEnemies = [new Enemy(-190, 57), new Enemy(-380, 143), new Enemy(-570, 233)];
+ // 3 enemies, each will start at a different position on the game screen
+// - 190, - 380, -570 pixel positions for each bug on the (x axis)
+//the bugs are at a negative position so they are not on the canvas here
+//on line (#31 update method) each bug walks at different positions on the canvas
+
+//map function can also be used in es6 (new feature )
+//declaring variable with array which holds all the 3 enemies on our game board, the bugs from the
+//array will be used to loop through each of the different positions of the bugs.
+//the engine.js file uses the enemies(bugs) from this allEnemies variable, map creates new allEnemies array and loops over
+
+
+function congratulations() {
+    reset();
+    alert("Congratulations Game Over!")
+    window.alert = function () { //removes the alert box, once player selects "ok"
+   
+
+    }
+}
+
+function reset() {
+    allEnemies = [];
+
+    
+
+
+}   
+
+
+//https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas
+function collisions(ex, ey, ew, eh, px, py, pw, ph) {
+    return (Math.abs(px - ex) * 2 < pw + ew) && (Math.abs(py - ey) * 2 < ph + eh);
+}
+
+//console.log(allEnemies); //ok making sure we have 3 enemies array
+
+                                          
+                                          // STEP#5 ***** EVENT LISTENER *****//
+        //***** includes: event listener function every time up, down, right, left arrows are clicked *****//
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+function refreshGame(){
+    window.location.reload();
+} 
+
+
+
+
+
+
+
+
+
+
